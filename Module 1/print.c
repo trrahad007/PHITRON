d6397e99a4d6dfc7c197c85cd7de94248db3ec15
trrahad007 <trrahad007@gmail.com>
2026-02-27 16:38:34 +0600

    Print.c added

---
+++ Module 1/print.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+   
+    printf("Tazwaar\n");
+    printf("/");
+    printf("\n Lets start for the journey of C programming\n");
+    return 0;
+}
--- Module 1/tazwaar.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    printf("Enter the number of elements: %d\n", n);
-    printf("Tazwaar\n");
-    return 0;
-}
