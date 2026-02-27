f67d0c67f09456fa83b7d1badcc5ab9d69ced89e
trrahad007 <trrahad007@gmail.com>
2026-02-27 16:41:55 +0600

    Datatypes.c added

---
+++ Module 1/datatypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    float b = 3.14;
+    char c = 'A';
+    printf("Integer: %d\n", a);
+    printf("Float: %.1f\n", b); // display float with 1 decimal place
+    printf("Character: %c\n", c);
+    return 0;
+}
